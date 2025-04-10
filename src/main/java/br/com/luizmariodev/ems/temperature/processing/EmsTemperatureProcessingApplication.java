8f4f2818f253fe7da4cc84416ef7b731a3612eef
Luiz Mário <azure@inforio.com.br>
2025-04-09 22:01:39 -0300

    Initial base code

---
+++ src/main/java/br/com/luizmariodev/ems/temperature/processing/EmsTemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTemperatureProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizmariodev/ems/temperature/processing/EmsTemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsTemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
